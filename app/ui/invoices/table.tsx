c7e5022febd7e43e72fae2cc9f0c8d31803c64b7
yuta925 <yutarou041@gmail.com>
2023-11-28 12:13:52 +0900

    chapter5

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -87,9 +87,10 @@ export default async function InvoicesTable({
                     <div className="flex items-center gap-3">
                       <Image
                         src={invoice.image_url}
-                        className="rounded-full"
                         width={28}
                         height={28}
+                        className="rounded-full"
+                        alt={`${invoice.name}'s profile picture`}
                       />
                       <p>{invoice.name}</p>
                     </div>
